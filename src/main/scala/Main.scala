a2aa347ca86c8c54766dfd1854ee6d385da91490
slowmetanoia <alexandervtarasovddd@gmail.com>
2026-01-20 16:40:11 +0300

    fact done

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,8 @@
+import scala.io.StdIn
 @main def hello(): Unit =
-  println("Hello world!")
-  println(msg)
+  def factorial(n:Int):Int = 
+    if(n<=0) 1 else factorial(n-1) * n
+  val x = StdIn.readInt()
+  println(factorial(x))
 
 def msg = "I was compiled by Scala 3. :)"
